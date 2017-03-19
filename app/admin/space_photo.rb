d24af80c1f9d1b57cf061bc673c28dd44e377a6e
Raghu Betina <raghu@firstdraft.com>
2017-03-19 00:54:20 +0000

    Whitelist columns in SpacePhoto for ActiveAdmin

--- app/admin/space_photo.rb
+++ app/admin/space_photo.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SpacePhoto do
 
+ permit_params :landlord_id, :space_id, :photo
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
