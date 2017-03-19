a0edb3cc933c636cb700712f67c2a879a37cf1c7
Raghu Betina <raghu@firstdraft.com>
2017-03-19 00:54:01 +0000

    Whitelist columns in Steward for ActiveAdmin

--- app/admin/steward.rb
+++ app/admin/steward.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Steward do
 
+ permit_params :telephone, :first_name, :last_name, :password
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
