b3b11c61637e7492d2056b1c6be4b8cb69138d11
Raghu Betina <raghu@firstdraft.com>
2017-03-19 00:54:15 +0000

    Whitelist columns in ReviewOfLandlord for ActiveAdmin

--- app/admin/review_of_landlord.rb
+++ app/admin/review_of_landlord.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ReviewOfLandlord do
 
+ permit_params :renter_id, :landlord_id, :rental_id, :landlord_rating, :landlord_notes, :landlord_response
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
